eb06d05501728d75c10dbe99f465e4a74036ffa9
Zhang Henghua <1652709417@qq.com>
2025-12-31 12:33:59 +0800

    init

--- v42/fields.go
+++ v42/fields.go
@@ -41,4 +41,46 @@ const (
 	// particularly those which electronically submit multi-day orders, trade globally or throughout market close
 	// periods,should ensure uniqueness across days, for example by embedding a date within the ClOrdID (11) field.
 	TagClOrdID
+
+	// TagCommission - Commission. Note if CommType (13) is percentage, Commission (12) of 5% should be represented as
+	// .05.
+	TagCommission
+
+	// TagCommType - Commission (12) type
+	TagCommType
+
+	// TagCumQty - Total number of shares filled.
+	TagCumQty
+
+	// TagCurrency - Identifies currency used for price. Absence of this field is interpreted as the default for the
+	// security. It is recommended that systems provide the currency value whenever possible.
+	TagCurrency
+
+	// TagEndSeqNo - Message sequence number of last message in range to be resent. If request is for a single message
+	// BeginSeqNo (7) = EndSeqNo (16). If request is for all messages subsequent to a particular message, EndSeqNo (16)
+	// = 0 (representing infinity).
+	TagEndSeqNo
+
+	// TagExecID - Unique identifier of execution message as assigned by broker (will be 0 (zero) for ExecTransType
+	// (20)='3' (Status)).
+	//
+	// Uniqueness must be guaranteed within a single trading day or the life of a multi-day order. Firms which accept
+	// multi-day orders should consider embedding a date within the ExecID (17) field to assure uniqueness across days.
+	TagExecID
+
+	// TagExecInst - Instructions for order handling on exchange trading floor. If more than one instruction is
+	// applicable to an order, this field can contain multiple instructions separated by space.
+	TagExecInst
+
+	// TagExecRefID - Reference identifier used with Cancel and Correct transaction types.
+	TagExecRefID
+
+	// TagExecTransType - Identifies transaction type
+	TagExecTransType
+
+	// TagHandlInst - Instructions for order handling on Broker trading floor
+	TagHandlInst
+
+	// TagIDSource - Identifies class of alternative SecurityID (48)
+	TagIDSource
 )
