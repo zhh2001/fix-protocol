f59bb540eeb3811f3ab4f7a856858d9674c58dd6
Zhang Henghua <1652709417@qq.com>
2026-01-02 15:24:10 +0800

    chore: Add several FIX protocol field constants

--- v42/enum.go
+++ v42/enum.go
@@ -24,6 +24,39 @@ const (
 	CommTypeAbsolute   = "3" // Absolute
 )
 
+// ExecInst
+const (
+	ExecInstStayOnOfferside            = "0" // Stay on offerside
+	ExecInstNotHeld                    = "1" // Not held
+	ExecInstWork                       = "2" // Work
+	ExecInstGoAlong                    = "3" // Go along
+	ExecInstOverTheDay                 = "4" // Over the day
+	ExecInstHeld                       = "5" // Held
+	ExecInstParticipateDontInitiate    = "6" // Participate don't initiate
+	ExecInstStrictScale                = "7" // Strict scale
+	ExecInstTryToScale                 = "8" // Try to scale
+	ExecInstStayOnBidside              = "9" // Stay on bidside
+	ExecInstNoCross                    = "A" // No cross (cross is forbidden)
+	ExecInstOKToCross                  = "B" // OK to cross
+	ExecInstCallFirst                  = "C" // Call first
+	ExecInstPercentOfVolume            = "D" // Percent of volume (indicates that the sender does not want to be all the volume on the floor vs. a specific percentage)
+	ExecInstDNI                        = "E" // Do not increase - DNI
+	ExecInstDNR                        = "F" // Do not reduce - DNR
+	ExecInstAON                        = "G" // All or none - AON
+	ExecInstInstitutionsOnly           = "I" // Institutions only
+	ExecInstLastPeg                    = "L" // Last peg (last sale)
+	ExecInstMidPricePeg                = "M" // Mid-price peg (midprice of inside quote)
+	ExecInstNonNegotiable              = "N" // Non-negotiable
+	ExecInstOpeningPeg                 = "O" // Opening peg
+	ExecInstMarketPeg                  = "P" // Market peg
+	ExecInstPrimaryPeg                 = "R" // Primary peg (primary market - buy at bid/sell at offer)
+	ExecInstSuspend                    = "S" // Suspend
+	ExecInstFixedPeg                   = "T" // Fixed Peg to Local best bid or offer at time of order
+	ExecInstCustomerDisplayInstruction = "U" // Customer Display Instruction (Rule11Ac1-1/4)
+	ExecInstNetting                    = "V" // Netting (for Forex)
+	ExecInstPegToVWAP                  = "W" // Peg to VWAP
+)
+
 // Side
 const (
 	SideBuy             = "1" // Buy
